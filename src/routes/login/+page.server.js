13a6f6cc280dfc952a2380c950223b1edbdcc5cd
Khairo Khatib <khairo.khatib@gmail.com>
2023-12-29 16:06:27 -0500

    feat: add login page and route

---
+++ src/routes/login/+page.server.js
@@ -0,0 +1,24 @@
+// @ts-nocheck
+/** @type {import('./$types').PageServerLoad} */
+import { error, redirect } from '@sveltejs/kit';
+
+export const actions = {
+	login: async ({ request, locals }) => {
+		const body = Object.fromEntries(await request.formData());
+
+		try {
+			await locals.pocketbase.collection('users').authWithPassword(body.email, body.password);
+			if (!locals.pocketbase?.authStore?.model?.verified) {
+				locals.pocketbase.authStore.clear();
+				return {
+					verified: false
+				};
+			}
+		} catch (err) {
+			console.log('Error: ', err);
+			throw error(500, 'An error occurred!');
+		}
+
+		throw redirect(303, '/');
+	}
+};
