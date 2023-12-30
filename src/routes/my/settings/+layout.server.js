43f34303e9c1bc195893833144ee6a1ba68bc885
Khairo Khatib <khairo.khatib@gmail.com>
2023-12-30 00:44:35 -0500

    feat: added server layout

--- src/routes/my/settings/+layout.server.js
+++ src/routes/my/settings/+layout.server.js
@@ -1,4 +1,9 @@
+// @ts-nocheck
+import { redirect } from '@sveltejs/kit';
+
 /** @type {import('./$types').LayoutServerLoad} */
-export async function load() {
-	return {};
-}
+export const load = ({ locals }) => {
+	if (!locals.pocketbase.authStore.isValid) {
+		throw redirect(303, '/login');
+	}
+};
